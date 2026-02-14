f6d5605971ccf74dffccf867f25d1e226fb6c008
Gautam Gupta <ggupta78@gmail.com>
2026-02-14 12:59:47 +0530

    add lecture 4 views 2 code

---
+++ lectures/lecture-4-views-2/Lecture4_Views2/app/src/main/java/com/example/lecture4_views2/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.lecture4_views2;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+  ArrayAdapter tvAdapter;
+
+  Spinner tvSpinner;
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    EdgeToEdge.enable(this);
+    setContentView(R.layout.activity_main);
+
+    tvSpinner = findViewById(R.id.spinner_tv);
+
+    tvAdapter = ArrayAdapter.createFromResource(this, R.array.tv_shows,
+      android.R.layout.simple_spinner_item);
+
+    tvAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+    tvSpinner.setAdapter(tvAdapter);
+
+
+    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main),
+      (v, insets) -> {
+        Insets systemBars = insets.getInsets(
+          WindowInsetsCompat.Type.systemBars());
+        v.setPadding(systemBars.left, systemBars.top, systemBars.right,
+          systemBars.bottom);
+        return insets;
+      });
+  }
+}
