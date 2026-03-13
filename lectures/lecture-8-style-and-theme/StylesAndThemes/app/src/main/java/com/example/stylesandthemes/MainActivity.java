786719efaa305c7a6bd523f75ec5f2a159bcb7c6
Gautam Gupta <ggupta78@gmail.com>
2026-03-13 13:23:52 +0530

    add style and theme app

---
+++ lectures/lecture-8-style-and-theme/StylesAndThemes/app/src/main/java/com/example/stylesandthemes/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.stylesandthemes;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+  Button btnStartSecondActivity;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    EdgeToEdge.enable(this);
+    setContentView(R.layout.activity_main);
+
+    btnStartSecondActivity = findViewById(R.id.btn_start_second_activity);
+
+    btnStartSecondActivity.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent i = new Intent(getApplicationContext(),
+          SecondActivity.class);
+        startActivity(i);
+      }
+    });
+
+    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main),
+      (v, insets) -> {
+        Insets systemBars = insets.getInsets(
+          WindowInsetsCompat.Type.systemBars());
+        v.setPadding(systemBars.left, systemBars.top, systemBars.right,
+          systemBars.bottom);
+        return insets;
+      });
+  }
+}
